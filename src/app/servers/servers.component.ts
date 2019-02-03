73c9205687146032e02c13e1e5147e2e1f7aaa9b
Matthew Zuniga <mzuniga94@csu.fullerton.edu>
2019-02-02 20:40:01 -0800

    Completed more lessons

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  //selector: '[app-servers]', // The [] means that it is like a CSS selector.
+  //selector: '.app-servers', // Acts like a selector of a div class.
   selector: 'app-servers',
   template: `<app-server></app-server>
              <app-server></app-server>`, // You can link to templateUrl or template but one must be present. Fine for small lines of code.
