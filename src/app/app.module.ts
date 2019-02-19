11b7383b4ebf2a99d7521900713cc66d7d3cc64e
Matthew Zuniga <mzuniga94@csu.fullerton.edu>
2019-02-18 22:59:33 -0800

    Learning data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Necessary for forms and two-way binding.
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -9,10 +10,12 @@ import { ServersComponent } from './servers/servers.component';
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,15 +4,30 @@ import { Component, OnInit } from '@angular/core';
   //selector: '[app-servers]', // The [] means that it is like a CSS selector.
   //selector: '.app-servers', // Acts like a selector of a div class.
   selector: 'app-servers',
-  template: `<app-server></app-server>
-             <app-server></app-server>`, // You can link to templateUrl or template but one must be present. Fine for small lines of code.
+  //template: `<app-server></app-server>
+  //           <app-server></app-server>`, // You can link to templateUrl or template but one must be present. Fine for small lines of code.
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+    allowNewServer = false;
+    serverCreationStatus = 'No server was created!';
+    serverName = 'Testserver';
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+        this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  } 
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
